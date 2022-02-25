ccdbf760c67849b165c1ae1d8d2765f24de4ffd1
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-25 18:30:34 +0530

    color picker

---
+++ src/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+        $("#btn1").on('click',function (){
+        $('html').css('background-color','green');
+        });
+
+        $("#btn2").on('click',function (){
+            $('html').css('background-color','blue');
+            });
+
+            $("#btn3").on('click',function (){
+                $('html').css('background-color','pink');
+                });
+
+                $("#btn4").on('click',function (){
+                    $('html').css('background-color','yellow');
+                    });
+            
+
+});
